d8732369761aec64130d6501487622f5d73bea74
Stephen Fox <sfox@stephenfoxdev.com>
2018-11-01 22:09:07 -0700

    design, build page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <main className={'main'}>
+      <section className={'main__left'}>
+        <h1 className={'title'}>Stephen Fox</h1>
+        <h2 className={'subtitle'}>Designer</h2>
+        <h2 className={'subtitle'}>Developer</h2>
+      </section>
+      <section className={'main__right'}>
+        <a className={'link'} target={'_blank'} href="https://www.linkedin.com/in/stephenfoxdev">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a className={'link'} target={'_blank'} href="https://twitter.com/manifoldkaizen">
+          <i className="fab fa-twitter-square"></i>
+        </a>
+        <a className={'link'} target={'_blank'} href="http://stephenfoxdev.com">
+          <i className="fas fa-external-link-square-alt"></i>
+        </a>
+      </section>
+    </main>
   </Layout>
 )
 
